85f03a5b6aaba7ed67c27f029ed5b5920864a7e3
Dailton Bastos <dailtonbastos@gmail.com>
2020-09-02 12:42:11 -0300

    feat: add fix products

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import product1 from '../../assets/images/product1.png';
+import product2 from '../../assets/images/product2.png';
+import product3 from '../../assets/images/product3.png';
+import product4 from '../../assets/images/product4.png';
 
 import './styles.css';
 
@@ -19,37 +22,107 @@ function Cart(props) {
         </button>
 
         <p>Sacola</p>
-        <span>{props.products.length} Itens</span>
+        <span>{props.products.length + 5} Itens</span>
 
         <div className='products'>
-          {props.products.length ? (
-            props.products.map((item) => {
-              const { product } = item;
-
-              return (
-                <div className='item' key={product.id + Math.random()}>
-                  <a href='/'>
-                    <img src={product.images[0].url} alt='Product name' />
-                    <div className='info'>
-                      <p>{product.title}</p>
-                      <span>R$ {product.price}</span>
-                    </div>
-                  </a>
-                  <div className='quantity-control'>
-                    <button type='button'>-</button>
-                    <span>{item.quantity}</span>
-                    <button type='button'>+</button>
+          {props.products &&
+            props.products.map((item) => (
+              <div className='item' key={item.product.id + Math.random()}>
+                <a href='/'>
+                  <img src={item.product.images[0].url} alt='Product name' />
+                  <div className='info'>
+                    <p>{item.product.title}</p>
+                    <span>R$ {item.product.price}</span>
                   </div>
-                  <button type='button'>X</button>
+                </a>
+                <div className='quantity-control'>
+                  <button type='button'>-</button>
+                  <span>{item.quantity}</span>
+                  <button type='button'>+</button>
                 </div>
-              );
-            })
-          ) : (
-            <div className='cart-empty'>
-              <p>Seu carrinho está vazio!</p>
-              <Link to='/'>Comprar agora</Link>
+                <button type='button'>X</button>
+              </div>
+            ))}
+
+          <div className='item'>
+            <a href='/'>
+              <img src={product1} alt='Product name' />
+              <div className='info'>
+                <p>Bolsa Bege Lorena</p>
+                <span>R$ 79,90</span>
+              </div>
+            </a>
+            <div className='quantity-control'>
+              <button type='button'>-</button>
+              <span>5</span>
+              <button type='button'>+</button>
             </div>
-          )}
+            <button type='button'>X</button>
+          </div>
+
+          <div className='item'>
+            <a href='/'>
+              <img src={product2} alt='Product name' />
+              <div className='info'>
+                <p>Rasteira Lara Areia</p>
+                <span>R$ 49,90</span>
+              </div>
+            </a>
+            <div className='quantity-control'>
+              <button type='button'>-</button>
+              <span>9</span>
+              <button type='button'>+</button>
+            </div>
+            <button type='button'>X</button>
+          </div>
+
+          <div className='item'>
+            <a href='/'>
+              <img src={product3} alt='Product name' />
+              <div className='info'>
+                <p>Bolsa Feminina de Ombro Pagani</p>
+                <span>R$ 99,95</span>
+              </div>
+            </a>
+            <div className='quantity-control'>
+              <button type='button'>-</button>
+              <span>2</span>
+              <button type='button'>+</button>
+            </div>
+            <button type='button'>X</button>
+          </div>
+
+          <div className='item'>
+            <a href='/'>
+              <img src={product4} alt='Product name' />
+              <div className='info'>
+                <p>Sandália Lara Areia</p>
+                <span>R$ 59,90</span>
+              </div>
+            </a>
+            <div className='quantity-control'>
+              <button type='button'>-</button>
+              <span>4</span>
+              <button type='button'>+</button>
+            </div>
+            <button type='button'>X</button>
+          </div>
+
+          <div className='item'>
+            <a href='/'>
+              <img src={product2} alt='Product name' />
+              <div className='info'>
+                <p>Rasteira Lara Areia</p>
+                <span>R$ 49,90</span>
+              </div>
+            </a>
+            <div className='quantity-control'>
+              <button type='button'>-</button>
+              <span>9</span>
+              <button type='button'>+</button>
+            </div>
+            <button type='button'>X</button>
+          </div>
         </div>
 
         <footer>
