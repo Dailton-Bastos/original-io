626aff63eca3960e31ee6e5e26ab29c892b43385
Dailton Bastos <dailtonbastos@gmail.com>
2020-09-07 18:15:22 -0300

    feat: cart mobile version

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import remove_icon from '../../assets/icons/remove_icon.svg'
+import add_icon from '../../assets/icons/add_icon.svg'
 import product1 from '../../assets/images/product1.png';
 import product2 from '../../assets/images/product2.png';
 import product3 from '../../assets/images/product3.png';
@@ -9,133 +11,218 @@ import './styles.css';
 
 function Cart(props) {
   return (
-    <section
-      className={props.openCart ? 'cart-container active' : 'cart-container'}
-    >
-      <div className='content'>
+    <section className={props.openCart ? 'cart-container isOpen' : 'cart-container'}>
+      <div className="cart-content">
         <button
-          type='button'
-          className='close'
+          type="button"
+          className="close-cart"
           onClick={() => props.setOpenCart(false)}
-        >
-          X
-        </button>
+        >X</button>
 
-        <p>Sacola</p>
-        <span>{props.products.length + 5} Itens</span>
+        <header>
+          <p>Sacola</p>
+          <span>{props.products.length + 5} Itens</span>
+        </header>
 
-        <div className='products'>
-          {props.products &&
-            props.products.map((item) => (
-              <div className='item' key={item.product.id + Math.random()}>
-                <a href='/'>
+        <div className="itens">
+          { props.products && props.products.map((item) => (
+            <div className="item" key={item.product.id + Math.random()}>
+              <div className="item-info">
+                <div className="image">
                   <img src={item.product.images[0].url} alt='Product name' />
-                  <div className='info'>
-                    <p>{item.product.title}</p>
-                    <span>R$ {item.product.price}</span>
-                  </div>
-                </a>
-                <div className='quantity-control'>
-                  <button type='button'>-</button>
-                  <span>{item.quantity}</span>
-                  <button type='button'>+</button>
                 </div>
-                <button type='button'>X</button>
+                <div className="detail">
+                  <p>{item.product.title}</p>
+                  <span>R$ {item.product.price}</span>
+                </div>
               </div>
-            ))}
 
-          <div className='item'>
-            <a href='/'>
-              <img src={product1} alt='Product name' />
-              <div className='info'>
+              <div className="item-increments">
+                <div className="add-remove">
+                  <span><img src={remove_icon} alt="Remove icon" /></span>
+                  <p className="total-item">{item.quantity}</p>
+                  <span><img src={add_icon} alt="Add icon" /></span>
+                </div>
+                <div className="delete-item">
+                  <button>X</button>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product1} alt='Product name' />
+              </div>
+              <div className="detail">
                 <p>Bolsa Bege Lorena</p>
                 <span>R$ 79,90</span>
               </div>
-            </a>
-            <div className='quantity-control'>
-              <button type='button'>-</button>
-              <span>5</span>
-              <button type='button'>+</button>
             </div>
-            <button type='button'>X</button>
+
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
+              </div>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
+            </div>
           </div>
 
-          <div className='item'>
-            <a href='/'>
-              <img src={product2} alt='Product name' />
-              <div className='info'>
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product2} alt='Product name' />
+              </div>
+              <div className="detail">
                 <p>Rasteira Lara Areia</p>
                 <span>R$ 49,90</span>
               </div>
-            </a>
-            <div className='quantity-control'>
-              <button type='button'>-</button>
-              <span>9</span>
-              <button type='button'>+</button>
             </div>
-            <button type='button'>X</button>
+
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
+              </div>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
+            </div>
           </div>
 
-          <div className='item'>
-            <a href='/'>
-              <img src={product3} alt='Product name' />
-              <div className='info'>
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product3} alt='Product name' />
+              </div>
+              <div className="detail">
                 <p>Bolsa Feminina de Ombro Pagani</p>
                 <span>R$ 99,95</span>
               </div>
-            </a>
-            <div className='quantity-control'>
-              <button type='button'>-</button>
-              <span>2</span>
-              <button type='button'>+</button>
             </div>
-            <button type='button'>X</button>
-          </div>
 
-          <div className='item'>
-            <a href='/'>
-              <img src={product4} alt='Product name' />
-              <div className='info'>
-                <p>Sandália Lara Areia</p>
-                <span>R$ 59,90</span>
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
               </div>
-            </a>
-            <div className='quantity-control'>
-              <button type='button'>-</button>
-              <span>4</span>
-              <button type='button'>+</button>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
             </div>
-            <button type='button'>X</button>
           </div>
 
-          <div className='item'>
-            <a href='/'>
-              <img src={product2} alt='Product name' />
-              <div className='info'>
-                <p>Rasteira Lara Areia</p>
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product4} alt='Product name' />
+              </div>
+              <div className="detail">
+                <p>Rasteira Tira Dedo</p>
                 <span>R$ 49,90</span>
               </div>
-            </a>
-            <div className='quantity-control'>
-              <button type='button'>-</button>
-              <span>9</span>
-              <button type='button'>+</button>
             </div>
-            <button type='button'>X</button>
+
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
+              </div>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product2} alt='Product name' />
+              </div>
+              <div className="detail">
+                <p>Rasteira Tira Dedo</p>
+                <span>R$ 49,90</span>
+              </div>
+            </div>
+
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
+              </div>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product2} alt='Product name' />
+              </div>
+              <div className="detail">
+                <p>Rasteira Tira Dedo</p>
+                <span>R$ 49,90</span>
+              </div>
+            </div>
+
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
+              </div>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="item-info">
+              <div className="image">
+                <img src={product2} alt='Product name' />
+              </div>
+              <div className="detail">
+                <p>Rasteira Tira Dedo</p>
+                <span>R$ 49,90</span>
+              </div>
+            </div>
+
+            <div className="item-increments">
+              <div className="add-remove">
+                <span><img src={remove_icon} alt="Remove icon" /></span>
+                <p className="total-item">01</p>
+                <span><img src={add_icon} alt="Add icon" /></span>
+              </div>
+              <div className="delete-item">
+                <button>X</button>
+              </div>
+            </div>
           </div>
         </div>
 
-        <footer>
-          <div className='promo'>
+        <footer className="cart-footer">
+          <div className="cart-promo">
             <p>
-              Faltam R$ xx,xx para você <span>Ganhar Frete Grátis</span>
+              Faltam R$ xx,xx para você <br />
+              <span>Ganhar Frete Grátis</span>
             </p>
           </div>
-
-          <div className='actions'>
-            <div className='resume'>
-              <p>Total: R$ 149,00</p>
-              <span>até 3x de R$ 49,90 sem juros</span>
+          <div className="cart-total-price">
+            <div className="cart-resume">
+              <span>Total: R$ 149,00</span>
+              <p>até 3x de R$ 49,90 sem juros</p>
             </div>
 
             <button type='button'>Finalizar Compra</button>
