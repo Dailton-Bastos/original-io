ee54e9b9546a9e4f9da8d97650d30c74a106d0a9
Dailton Bastos <dailtonbastos@gmail.com>
2020-09-07 18:16:23 -0300

    feat: lightbox mobile version

--- src/components/Lightbox/index.js
+++ src/components/Lightbox/index.js
@@ -1,34 +1,34 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 function Lightbox(props) {
   return (
     <section
-      className={
-        props.openLightbox ? 'lightbox-container active' : 'lightbox-container'
+      className={`${props.openLightbox ? 'ligthbox active' : 'ligthbox'}`
       }
     >
       <div className='content'>
         <button
           type='button'
-          className='close'
+          className='btn-close'
           onClick={() => props.setOpenLightbox(false)}
         >
           X
         </button>
 
-        <div className='details'>
+        <h1>
+          Produto adicionado <br /> com sucesso!
+        </h1>
+
+        <div className="image-preview">
           <img src={props.product?.images?.[0].url} alt='Product name' />
-
-          <p>
-            Produto adicionado <br /> com sucesso!
-          </p>
-
-          <button type='button'>Finalizar Compra</button>
-
-          <a href='/'>Continuar Comprando</a>
         </div>
+
+        <button type='button' className="btn-buy">Finalizar Compra</button>
+        <Link to="/">Continuar Comprando</Link>
       </div>
     </section>
   );
